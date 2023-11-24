5cd75fc966765bd13aa91f29b67d5a1d5ab37124
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-11-24 12:32:59 -0600

    added more complexity

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello, World! - Iteration " + (i + 1));
+        }
     }
 }
