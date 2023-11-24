10eeebe6df5788819db0a4643753e63c6f917d5d
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-11-24 12:42:45 -0600

    added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,33 +1,27 @@
-import java.util.Scanner;
-
 public class HelloWorld {
     public static void main(String[] args) {
-        // Print a welcome message
-        System.out.println("Welcome to the Complex HelloWorld Program!");
+        System.out.println("Welcome to the Enhanced HelloWorld Program!");
+        
+        // Display a message with a variable
+        String message = "Hello, World!";
+        System.out.println(message);
 
-        // Get user input for their name
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String userName = scanner.nextLine();
-
-        // Greet the user
-        System.out.println("Hello, " + userName + "!");
-
-        // Demonstrate a loop with user interaction
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Iteration " + (i + 1) + ": Have a great day, " + userName + "!");
-            askForFeedback();
+        // Display numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("Number: " + i);
         }
 
-        // Display a farewell message
-        System.out.println("Thank you for using the Complex HelloWorld Program!");
+        // Perform a simple calculation
+        int result = addNumbers(5, 7);
+        System.out.println("Result of adding 5 and 7: " + result);
+
+        // Display a closing message
+        System.out.println("Thank you for using the Enhanced HelloWorld Program!");
     }
 
-    // Method to ask the user for feedback
-    private static void askForFeedback() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Did you enjoy the greeting? (yes/no): ");
-        String feedback = scanner.nextLine();
-        System.out.println("Feedback: " + feedback);
+    // Method to add two numbers
+    private static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
+
