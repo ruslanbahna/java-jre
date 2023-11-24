b1b1e2c5ac50b6edb83ed81b80c6466c41cb41f3
Ruslan Bahna <120433135+ruslanbahna@users.noreply.github.com>
2023-11-24 12:38:56 -0600

    more complex

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,28 +1,33 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         // Print a welcome message
-        System.out.println("Welcome to the Extended HelloWorld Program!");
+        System.out.println("Welcome to the Complex HelloWorld Program!");
 
-        // Loop to print "Hello, World!" multiple times
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Hello, World! - Iteration " + (i + 1));
+        // Get user input for their name
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String userName = scanner.nextLine();
+
+        // Greet the user
+        System.out.println("Hello, " + userName + "!");
+
+        // Demonstrate a loop with user interaction
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Iteration " + (i + 1) + ": Have a great day, " + userName + "!");
+            askForFeedback();
         }
 
-        // Call a method to demonstrate function invocation
-        printCustomMessage("This is a custom message!");
-
-        // Calculate and print the sum of numbers
-        int sum = calculateSum(5, 10);
-        System.out.println("The sum of 5 and 10 is: " + sum);
+        // Display a farewell message
+        System.out.println("Thank you for using the Complex HelloWorld Program!");
     }
 
-    // Method to print a custom message
-    private static void printCustomMessage(String message) {
-        System.out.println("Custom Message: " + message);
-    }
-
-    // Method to calculate the sum of two numbers
-    private static int calculateSum(int a, int b) {
-        return a + b;
+    // Method to ask the user for feedback
+    private static void askForFeedback() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Did you enjoy the greeting? (yes/no): ");
+        String feedback = scanner.nextLine();
+        System.out.println("Feedback: " + feedback);
     }
 }
